f5b0ce52cd7ca804c011c517173caefa5c7c4f2c
Prithsray <tatairay6@gmail.com>
2024-06-23 01:15:25 +0530

    docker file updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start Rasa server
-# rasa run --cors "*" --enable-api --port 5005 --debug &
+rasa run --cors "*" --enable-api --port 5005 --debug &
 
-# # Start Rasa action server
-# rasa run actions --port 5055 --debug &
+# Start Rasa action server
+rasa run actions --port 5055 --debug &
 
 # Start Flask app with Gunicorn
 gunicorn -w 4 -b 0.0.0.0:8000 app:app
